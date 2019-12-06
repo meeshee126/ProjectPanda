54912ee5f7be45aadaa24175c28a219077ccc4b6
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-12-06 23:54:30 +0100

    shader things

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -9,18 +9,50 @@ public class Destroy : MonoBehaviour
     public Vector3 position;
     public Vector3 rotation;
 
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
         position = this.transform.position;
         rotation = this.transform.eulerAngles;
     }
-
+    /*
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.name == "Panda")
         {
+          // this.gameObject.SetActive(false);
+
+            
+
+            BoxCollider[] colliders = GetComponents<BoxCollider>();
+
+            foreach (BoxCollider collider in colliders)
+            {
+                collider.enabled = false;
+            }
             Instantiate(destroyed, transform.position, Quaternion.Euler(rotation));
             Destroy(this.gameObject);
         }
+    }
+    */
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.collider.name == "Panda")
+        {
+            Rigidbody[] rbs = GetComponentsInChildren<Rigidbody>();
+
+            foreach (Rigidbody rb in rbs)
+            {
+                rb.isKinematic = false;
+            }
+        }     
     }
 }
