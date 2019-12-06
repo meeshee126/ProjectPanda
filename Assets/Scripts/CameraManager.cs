c06c1a448ed9449712aa7a65b3799a25e88807d2
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-12-06 16:27:26 +0100

    Initial commit

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    Transform panda;
+
+    [SerializeField]
+    float lerpSpeed, xOffset, yOffset, zOffset;
+
+    void Start()
+    {
+      
+
+        panda = GameObject.Find("Panda").GetComponent<Transform>();
+
+        this.transform.position = panda.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPanda();
+    }
+
+    public void FollowPanda()
+    {
+        Vector3 offset = new Vector3(xOffset, yOffset, zOffset);
+
+        Vector3 lerp = Vector3.Lerp(transform.position, panda.position + offset, lerpSpeed * Time.deltaTime);
+
+        transform.position = lerp;
+
+        transform.LookAt(panda.position);
+    }
+}
---
+++ Assets/Scripts/Panda.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panda : MonoBehaviour
+{
+    [SerializeField]
+    float tourqueForce;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        rb.AddTorque(new Vector3(moveX, 0, moveZ) * tourqueForce * Time.deltaTime);
+    }
+}
