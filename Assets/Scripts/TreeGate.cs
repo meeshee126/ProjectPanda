ffb6f7550194360a9a281f535881841fd977785e
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-12-06 22:31:14 +0100

    Baggerrrrr

---
+++ Assets/Scripts/TreeGate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGate : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Panda")
+        {
+            Transform[] trees = GetComponentsInChildren<Transform>();
+
+            foreach (Transform tree in trees)
+            {
+                if (tree.name.Contains("_tree"))
+                {
+                    tree.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                }
+            }
+
+            Destroy(this);
+        }
+    }
+}
