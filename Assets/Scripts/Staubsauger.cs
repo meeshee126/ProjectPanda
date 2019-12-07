4908fe1b63b389cdf47197f82fa5b0f94bbc3618
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-12-07 16:44:41 +0100

    skybox

---
+++ Assets/Scripts/Staubsauger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staubsauger : MonoBehaviour
+{
+    public GameObject camera;
+
+    private void Update()
+    {
+        transform.position = camera.transform.position + new Vector3(30, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Object")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
