0103bd9adf3c28c933256ac1959267b8e78638ec
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-12-07 18:01:04 +0100

    push

---
+++ Assets/NotDestroyObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotDestroyObject : MonoBehaviour
+{
+    Rigidbody rb;
+    public BoxCollider boxCollider;
+
+    public float speed;
+    bool hitted;
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (!hitted)
+        {
+            // rb.velocity  = (Vector3.down * speed * Time.deltaTime);
+        }
+        else
+        {
+            rb.velocity = new Vector3();
+            boxCollider.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ramp")
+        {
+            boxCollider.enabled = false;
+            hitted = true;
+            rb.velocity = new Vector3();
+            rb.isKinematic = true;
+            Debug.Log("hit");
+
+            BoxCollider[] colliders = GetComponentsInChildren<BoxCollider>();
+
+            if (colliders.Length != 0)
+            {
+                foreach (BoxCollider collider in colliders)
+                {
+                    collider.enabled = true;
+                }
+            }
+
+
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/DestroyRamp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyRamp : MonoBehaviour
+{
+    GameObject panda;
+
+    void Start()
+    {
+        panda = GameObject.Find("Panda");
+    }
+
+    void Update()
+    {
+        if(Vector3.Distance(this.transform.position, panda.transform.position) > 190)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
