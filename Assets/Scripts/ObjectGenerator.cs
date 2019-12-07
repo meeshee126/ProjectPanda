b9909f85aa3306fc0f28b71a408f4c4725c2b935
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-12-07 02:02:46 +0100

    schilder

---
+++ Assets/Scripts/ObjectGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGenerator : MonoBehaviour
+{
+    public List<GameObject> objects = new List<GameObject>();
+
+    public Vector3 offset;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, offset);
+    }
+}
